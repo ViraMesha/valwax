ed09caeb1ea8b2a5623685edeef23d227c227c17
Anton Shabalov <shabalovanton@gmail.com>
2023-07-26 12:53:48 +0300

    fix: returned Instagram.tsx

---
+++ src/components/Instagram/Instagram.tsx
@@ -0,0 +1,21 @@
+import Typography from '../Typography/Typography'
+import styles from './Instagram.module.css'
+
+const Instagram = () => {
+  return (
+    <>
+      <Typography
+      variant='subheading1'
+      color="var(--cl-primary-800)"
+      >
+        Instagram
+      </Typography>
+      {/* <h2 className={styles.txt}>Instagram section</h2> */}
+      {/* <h2 className={avenirBook.className}>"СВІЧКА, запалена з любові, може висвітлити темні куточки"</h2> */}
+
+    </>
+  )
+}
+
+
+export default Instagram
