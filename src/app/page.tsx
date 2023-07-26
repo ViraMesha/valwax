c3c57214d5e77f3088128ffcf2a6f5456e50f3c2
ViraMesha <wira.mescha@gmail.com>
2023-07-27 00:18:19 +0300

    add some layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
-import Image from "next/image";
-import Link from "next/link";
-import AboutUsSection from "@components/components/AboutUsSection/AboutUsSection";
+import Image from 'next/image';
+import Link from 'next/link';
+import AboutUsSection from '@components/components/AboutUsSection/AboutUsSection';
 // приклад використання типографії і другого фонту
 // import Typography from "@components/components/Typography/Typography";
 // import { avenir } from "./fonts";
-import Button from "@components/components/Button/Button";
-import Navigation from "@components/components/Navigation/Navigation";
-import Quote from "@components/components/Quote/Quote";
+import Button from '@components/components/Button/Button';
+import Header from '@components/components/Header/Header';
+import Navigation from '@components/components/Navigation/Navigation';
+import Quote from '@components/components/Quote/Quote';
 
-import Compass from "../components/Compass/Compass";
-import Instagram from "../components/Instagram/Instagram";
+import Compass from '../components/Compass/Compass';
 
-import styles from "./page.module.css";
+import Instagram from './../components/instagram/Instagram';
+
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Home page</h1>
-      <AboutUsSection />
-      <Quote />
-      {/* <Typography
+    <>
+      <Header />
+      <main className={styles.main}>
+        <h1>Home page</h1>
+        <AboutUsSection />
+        <Quote />
+        {/* <Typography
         variant="heading1"
         className={avenir.className}
         color="var(--cl-secondary-900)"
@@ -27,20 +31,20 @@ export default function Home() {
         Запалюйте світло
       </Typography> */}
 
-      <Button variant="primary">Переглянути Каталог</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="tertiary" hasIcon>
-        Button Text
-      </Button>
-      <nav>
-        <Link href="/candle">Candle page</Link>
-      </nav>
+        <Button variant="primary">Переглянути Каталог</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="tertiary" hasIcon>
+          Button Text
+        </Button>
+        <nav>
+          <Link href="/candle">Candle page</Link>
+        </nav>
 
-      <Instagram />
-      <Compass />
+        <Instagram />
+        <Compass />
 
-      <Navigation/>
-      
-    </main>
+        <Navigation />
+      </main>
+    </>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BiShoppingBag } from 'react-icons/bi';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TbCurrencyHryvnia } from 'react-icons/tb';
 
+import flagUA from '../../../public/images/icons/flag-ukraine.svg';
 import logo from '../../../public/images/icons/header-logo.svg';
 import Container from '../Container/Container';
 import Navigation from '../Navigation/Navigation';
@@ -12,12 +17,29 @@ const Header = () => {
     <header className={styles.header}>
       <Container className={styles.headerContainer}>
         <Link href="/" className={styles.logo}>
-          <Image src={logo} alt="logo" priority />
+          <Image src={logo} alt="Logo" priority />
         </Link>
         <Navigation />
-        <ul>
-          <li></li>
-        </ul>
+        <div className={styles.icons}>
+          <ul className={styles.shoppingIcons}>
+            <li className={styles.shoppingIcons__item}>
+              <BiShoppingBag style={{ width: '24px', height: '24px' }} />
+            </li>
+            <li className={styles.shoppingIcons__item}>
+              <AiOutlineSearch style={{ width: '24px', height: '24px' }} />
+            </li>
+          </ul>
+          <ul>
+            <li className={styles.language_menu}>
+              <Image src={flagUA} alt="Ukrainian flag" />
+              <TbCurrencyHryvnia style={{ width: '24px', height: '24px' }} />
+              <MdKeyboardArrowDown
+                className={styles.arrowDown}
+                style={{ width: '24px', height: '24px' }}
+              />
+            </li>
+          </ul>
+        </div>
       </Container>
     </header>
   );
