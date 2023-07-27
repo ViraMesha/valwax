05bb5f12aa105fbddd55989c06d9e445f4937e43
ViraMesha <wira.mescha@gmail.com>
2023-07-27 15:24:48 +0300

    add styles for Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,35 +4,40 @@ import { avenir } from '@components/app/fonts';
 import heroBg from '../../../public/images/hero/hero.jpg';
 import Button from '../Button/Button';
 import Container from '../Container/Container';
+import Section from '../Section/Section';
 import Typography from '../Typography/Typography';
 
 import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <section className={styles.sectionWrapper}>
-      <Image
-        src={heroBg.src}
-        alt="Background Image"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-      />
+    <Section className={styles.sectionWrapper}>
       <Container>
-        <div className={styles.heading}>
-          <Typography variant="heading1" className={avenir.className}>
-            Запалюйте світло,
-          </Typography>
-          <Typography variant="heading1" className={avenir.className}>
-            пробуджуйте емоції,
-          </Typography>
-          <Typography variant="heading1" className={avenir.className}>
-            творіть натхнення!
-          </Typography>
+        <Image
+          src={heroBg.src}
+          alt="Background Image"
+          width={1200}
+          height={650}
+          quality={100}
+          priority
+          className={styles.bgImage}
+        />
+        <div className={styles.content}>
+          <div className={styles.heading}>
+            <Typography variant="heading1" className={avenir.className}>
+              Запалюйте світло,
+            </Typography>
+            <Typography variant="heading1" className={avenir.className}>
+              пробуджуйте емоції,
+            </Typography>
+            <Typography variant="heading1" className={avenir.className}>
+              творіть натхнення!
+            </Typography>
+          </div>
+          <Button variant="primary">Переглянути Каталог</Button>
         </div>
-        <Button variant="primary">Переглянути Каталог</Button>
       </Container>
-    </section>
+    </Section>
   );
 };
 
