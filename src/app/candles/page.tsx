157d6038fdf69440aff676c2912a2ea43f111500
ViraMesha <wira.mescha@gmail.com>
2023-08-21 22:14:30 +0300

    feat: add Container to Breadcrumbs

--- src/app/candles/page.tsx
+++ src/app/candles/page.tsx
@@ -3,7 +3,6 @@ import Breadcrumbs from '@components/components/Breadcrumbs/Breadcrumbs';
 const Candle = () => {
   return (
     <>
-      <h2>Candles page</h2>
       <Breadcrumbs
         items={[
           {
--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { BiSolidChevronRight } from 'react-icons/bi';
 
 import home from '../../../public/images/icons/home-breadcrumbs-heart.svg';
+import Container from '../Container/Container';
 
 import { BreadcrumbsProps } from './data';
 
@@ -10,26 +11,28 @@ import styles from './Breadcrumbs.module.css';
 
 const Breadcrumbs = ({ items }: BreadcrumbsProps) => {
   return (
-    <nav aria-label="breadcrumbs">
-      <ol className={styles.container}>
-        <li className={styles.crumb}>
-          <Link href="/" className={`${styles.link} ${styles.noHover}`}>
-            <Image src={home} alt="Home" width={24} height={24} />
-          </Link>
-          <BiSolidChevronRight className={styles.separator} />
-        </li>
-        {items.map((crumb, i) => (
-          <li key={i} className={styles.crumb}>
-            <Link href={crumb.path} className={`${styles.link}`}>
-              {crumb.label}
+    <Container>
+      <nav aria-label="breadcrumbs">
+        <ol className={styles.container}>
+          <li className={styles.crumb}>
+            <Link href="/" className={`${styles.link} ${styles.noHover}`}>
+              <Image src={home} alt="Home" width={24} height={24} />
             </Link>
-            {items?.length - 1 !== i && (
-              <BiSolidChevronRight className={styles.separator} />
-            )}
+            <BiSolidChevronRight className={styles.separator} />
           </li>
-        ))}
-      </ol>
-    </nav>
+          {items.map((crumb, i) => (
+            <li key={i} className={styles.crumb}>
+              <Link href={crumb.path} className={`${styles.link}`}>
+                {crumb.label}
+              </Link>
+              {items?.length - 1 !== i && (
+                <BiSolidChevronRight className={styles.separator} />
+              )}
+            </li>
+          ))}
+        </ol>
+      </nav>
+    </Container>
   );
 };
 
