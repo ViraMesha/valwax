a622839145f4e07e7073350fd9838c54ca163b85
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-08-23 20:01:23 +0300

    added mixins, added adaptive subscription section

--- src/components/Subscription/Subscription.tsx
+++ src/components/Subscription/Subscription.tsx
@@ -4,25 +4,31 @@ import Input from '../Input/Input';
 import Section from '../Section/Section';
 import Typography from '../Typography/Typography';
 
-import styles from './Subscription.module.css';
-
+import styles from './Subscription.module.scss';
 const Subscription = () => {
+  
   return (
     <Section className={styles.subscription}>
       <Container>
         <Typography
           variant="subheading1"
           color="var(--cl-gray-700)"
-          className={styles.subscriptionTypography}
+          className={styles.subscriptionTitle}
         >
           Підпишись на нашу розсилку
         </Typography>
-        <Typography variant="bodyRegular" color="var(--cl-gray-700)">
+        <Typography
+          variant="bodyRegular"
+          color="var(--cl-gray-700)"
+          className={styles.subscriptionTypography}
+        >
           Отримуй ексклюзивні пропозиції та будь у курсі всіх наших новинок
         </Typography>
         <div className={styles.subscriptionWrapper}>
-          <Input width="400px" height="60px" placeholder="Email" />
-          <Button variant="primary">Підписатися</Button>
+          <Input placeholder="Email" />
+          <Button variant="primary" className={styles.subscriptionButton}>
+            Підписатися
+          </Button>
         </div>
       </Container>
     </Section>
