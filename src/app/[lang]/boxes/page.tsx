7d741c3706fb013e3ac2324b5a9994d472efdbe4
ViraMesha <wira.mescha@gmail.com>
2023-09-10 13:38:02 +0300

    feat: Add translation support for Breadcrumbs

--- src/app/[lang]/boxes/page.tsx
+++ src/app/[lang]/boxes/page.tsx
@@ -3,16 +3,21 @@ import BoxesPageHeader from '@components/components/BoxesPage/BoxesPageHeader/Bo
 import BoxesSection from '@components/components/BoxesPage/BoxesSection/BoxesSection';
 import Breadcrumbs from '@components/components/Breadcrumbs/Breadcrumbs';
 
-const Boxes = () => {
+import { Locale } from '../../../../i18n-config';
+import { getDictionary } from '../../../../lib/dictionary';
+
+const Boxes = async ({ params: { lang } }: { params: { lang: Locale } }) => {
+  const { breadcrumbs } = await getDictionary(lang);
   return (
     <>
       <Breadcrumbs
         items={[
           {
-            label: 'Подарункові бокси',
+            label: breadcrumbs.boxes,
             path: '/boxes',
           },
         ]}
+        lang={lang}
       />
       <BoxesPageHeader />
       <BoxesSection />
--- src/app/[lang]/candles/page.tsx
+++ src/app/[lang]/candles/page.tsx
@@ -1,16 +1,21 @@
 import Breadcrumbs from '@components/components/Breadcrumbs/Breadcrumbs';
 import CandlePage from '@components/components/CandlePage/CandlePage';
 
-const Candles = () => {
+import { Locale } from '../../../../i18n-config';
+import { getDictionary } from '../../../../lib/dictionary';
+
+const Candles = async ({ params: { lang } }: { params: { lang: Locale } }) => {
+  const { breadcrumbs } = await getDictionary(lang);
   return (
     <>
       <Breadcrumbs
         items={[
           {
-            label: 'Ароматичні соєві свічки',
-            path: '/candles',
+            label: breadcrumbs.soyCandles,
+            path: `/candles`,
           },
         ]}
+        lang={lang}
       />
       <CandlePage />
     </>
