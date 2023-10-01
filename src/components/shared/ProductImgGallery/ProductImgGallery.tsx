d8f7e6ed4532c552f529754b0892d98fbab2a76f
ViraMesha <wira.mescha@gmail.com>
2023-10-01 20:58:58 +0300

    feat: Add styles for mobile and tablet devices

--- src/components/shared/ProductImgGallery/ProductImgGallery.tsx
+++ src/components/shared/ProductImgGallery/ProductImgGallery.tsx
@@ -8,37 +8,34 @@ import styles from './ProductImgGallery.module.scss';
 
 const ProductImgGallery = ({ images }: { images: string[] }) => {
   const totalImages = images.length;
-  const [activeImageIndex, setActiveImageIndex] = useState(0);
 
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
   const initialPercentage = ((activeImageIndex + 1) / totalImages) * 100;
 
   const [progressBarHeight, setProgressBarHeight] = useState(initialPercentage);
-  const slider1Ref = useRef<Slider | null>(null);
+  const sliderRef = useRef<Slider | null>(null);
 
   const handleSmallImageClick = (index: number) => {
     setActiveImageIndex(index);
     const newPercentage = ((index + 1) / totalImages) * 100;
     setProgressBarHeight(newPercentage);
-    slider1Ref.current?.slickGoTo(index);
+    sliderRef.current?.slickGoTo(index);
   };
 
   return (
     <div className={styles.product_detail_images__wrapper}>
-      <div className={styles.product_detail_images__img_container}>
+      <div className={styles.slider__img_container}>
         <ReusableSlider
           vertical
           verticalSwiping
-          ref={slider1Ref}
+          ref={sliderRef}
           swipeToSlide={true}
           afterChange={(index: number) => handleSmallImageClick(index)}
         >
           {images.map((imageSrc, i) => (
-            <div
-              key={i}
-              className={styles.product_detail_images__img_inner_container}
-            >
+            <div key={i} className={styles.slider__img_inner_container}>
               <Image
-                className={styles.product_detail_images__image}
+                className={styles.slider__image}
                 src={imageSrc}
                 priority
                 fill
@@ -55,11 +52,11 @@ const ProductImgGallery = ({ images }: { images: string[] }) => {
           style={{ height: `${progressBarHeight}%` }}
         ></div>
       </div>
-      <div className={styles.small_img_container}>
+      <div className={styles.product_detail_images__container}>
         {images.map((imageSrc, index) => (
           <div
             key={index}
-            className={styles.small_img_inner_container}
+            className={styles.product_detail_images__inner_container}
             onClick={() => handleSmallImageClick(index)}
           >
             <Image
