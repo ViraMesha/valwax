1625bf0073ee807e5fb7bc4c811ed374febb9c0b
ViraMesha <wira.mescha@gmail.com>
2023-10-16 17:11:36 +0300

    refactor: fix the mobile menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AiOutlineClose, AiOutlineSearch } from 'react-icons/ai';
 import { HiOutlineMenuAlt1 } from 'react-icons/hi';
 import useModal from '@components/hooks/useModal';
@@ -30,6 +30,14 @@ const Header = ({ lang }: { lang: Locale }) => {
   const toggleMenuOpen = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
+  useEffect(() => {
+    const html = document.querySelector('html');
+
+    isMobileMenuOpen
+      ? html?.classList.add(styles.overflowHidden)
+      : html?.classList.remove(styles.overflowHidden);
+  }, [isMobileMenuOpen]);
 
   return (
     <header className={styles.header}>
