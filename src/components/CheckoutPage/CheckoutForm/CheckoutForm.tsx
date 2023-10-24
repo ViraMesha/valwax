d00dab7adb9b7beb38ceb6e53853e53168d889ee
ViraMesha <wira.mescha@gmail.com>
2023-10-24 23:46:39 +0300

    feat: add functionality to CheckoutForm

--- src/components/CheckoutPage/CheckoutForm/CheckoutForm.tsx
+++ src/components/CheckoutPage/CheckoutForm/CheckoutForm.tsx
@@ -1,10 +1,63 @@
+'use client';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { PhoneInput } from 'react-international-phone';
+import Button from '@components/components/Button/Button';
 import Input from '@components/components/Input/Input';
+import validationSchema from '@components/helpers/formValidationSchema';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { PhoneNumberUtil } from 'google-libphonenumber';
 
+import 'react-international-phone/style.css';
 import styles from './CheckoutForm.module.scss';
 
+const phoneUtil = PhoneNumberUtil.getInstance();
+
+const validatePhone = (phone: string) => {
+  try {
+    return phoneUtil.isValidNumber(phoneUtil.parseAndKeepRawInput(phone));
+  } catch (error) {
+    return false;
+  }
+};
+
+type CheckoutFormValues = {
+  // cashOnDelivery?: boolean | undefined;
+  // cardPayment?: boolean | undefined;
+  // comment?: string | undefined;
+  phone?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  // post: string;
+  // deliveryArea: string;
+  // deliveryCity: string;
+  // postOfficeBranchNum: string;
+};
+
 const CheckoutForm = () => {
+  const [phone, setPhone] = useState('');
+  const [isValidPhone, setIsValidPhone] = useState(true);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    setError,
+    setValue,
+  } = useForm<CheckoutFormValues>({
+    mode: 'onBlur',
+    defaultValues: {},
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data: CheckoutFormValues) => {
+    data.phone = phone;
+    console.log(data);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <fieldset className={styles.form__group}>
         <legend className={styles.group__title}>Контактні дані</legend>
         <div className={styles.contactInfo__wrapper}>
@@ -13,12 +66,18 @@ const CheckoutForm = () => {
             placeholder={`Ім’я`}
             id="user_firstname"
             required
+            errorMessage={errors.firstName?.message}
+            error={errors.firstName}
+            {...register('firstName')}
           />
           <Input
             label="Прізвище *"
             placeholder="Прізвище"
             id="user_lastname"
             required
+            errorMessage={errors.lastName?.message}
+            error={errors.lastName}
+            {...register('lastName')}
           />
           <Input
             label="Email *"
@@ -26,9 +85,37 @@ const CheckoutForm = () => {
             placeholder="Email"
             id="user_email"
             required
+            errorMessage={errors.email?.message}
+            error={errors.email}
+            {...register('email')}
           />
+
+          <div>
+            <label className={styles.label} htmlFor="phone">
+              Введіть номер телефону
+            </label>
+            <PhoneInput
+              inputProps={{
+                id: 'phone',
+              }}
+              defaultCountry="ua"
+              value={phone}
+              showDisabledDialCodeAndPrefix
+              disableDialCodeAndPrefix
+              className={styles.phoneInput__container}
+              onChange={phone => setPhone(phone)}
+              placeholder="93-000-00-00"
+              onBlur={() => setIsValidPhone(validatePhone(phone))}
+            />
+            {!isValidPhone && (
+              <p className={styles.error}>Phone is not valid</p>
+            )}
+          </div>
         </div>
       </fieldset>
+      <Button variant="primary" type="submit" className={styles.button}>
+        Оформити замовлення
+      </Button>
     </form>
   );
 };
