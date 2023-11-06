e438114f5e983aa470db5afe49e4a0946c960d70
ViraMesha <wira.mescha@gmail.com>
2023-11-07 00:18:54 +0200

    feat: Add styles to NotFound page

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -1,14 +1,22 @@
 'use client';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import Container from '@components/components/Container/Container';
+import Section from '@components/components/Section/Section';
 import CustomLink from '@components/components/shared/CustomLink/CustomLink';
+import Typography from '@components/components/Typography/Typography';
 
 import { Locale } from '../../../i18n-config';
 import { getDictionary } from '../../../lib/utils/dictionary';
+import notFoundImg from '../../../public/images/icons/not-found.svg';
+
+import styles from './not-found.module.scss';
 
 interface DictionaryState {
   text: string;
   linkText: string;
+  altText: string;
 }
 
 export default function NotFound() {
@@ -30,12 +38,32 @@ export default function NotFound() {
   }, [lang]);
 
   return (
-    <div>
-      <h2>404</h2>
-      <p>{dictionary?.text}</p>
-      <CustomLink variant="secondary" href={`/`}>
-        {dictionary?.linkText}
-      </CustomLink>
-    </div>
+    <Section id={styles.notFound}>
+      <Container className={styles.notFound__container}>
+        <Typography variant="subheding4" color="var(--cl-primary-500)">
+          404
+        </Typography>
+        <Typography
+          variant="bodyRegular"
+          color="var(--cl-gray-500)"
+          className={styles.notFound__text}
+        >
+          {dictionary?.text}
+        </Typography>
+        <Image
+          src={notFoundImg}
+          alt={
+            dictionary?.altText ||
+            'An image of a young boy holding a lit candle close to his chest, creating a warm and intimate atmosphere.'
+          }
+          sizes="(min-width: 1230px) 190px, 
+          (min-width: 768px) 198px,
+          110px"
+        />
+        <CustomLink variant="secondary" href={`/${lang}`}>
+          {dictionary?.linkText}
+        </CustomLink>
+      </Container>
+    </Section>
   );
 }
