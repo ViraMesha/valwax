2b15936fdba67937b9f85411311b9ba3bebe7db1
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-11-11 18:27:12 +0200

    updated page styles to match the design

--- src/components/PaymentAndDeliveryInfo/PaymentAndDeliveryInfo.tsx
+++ src/components/PaymentAndDeliveryInfo/PaymentAndDeliveryInfo.tsx
@@ -1,3 +1,7 @@
+import { BsBoxSeam } from 'react-icons/bs';
+// import { FaCreditCard } from 'react-icons/fa';
+import { BsCreditCard } from 'react-icons/bs';
+
 import Container from '../Container/Container';
 import Section from '../Section/Section';
 import Typography from '../Typography/Typography';
@@ -6,21 +10,77 @@ import styles from './PaymentAndDeliveryInfo.module.scss';
 
 interface PaymentAndDeliveryInfoProps {
   dict: {
-    subTitle: string;
-    texts: string[];
-  }[];
+    header: {
+      title: string;
+    };
+    paymentOptions: {
+      subTitle: string;
+      description: string;
+    };
+    deliveryOptions: {
+      subTitle: string;
+      time: string;
+      methods: {
+        title: string;
+        availableMethods: string[];
+      };
+      paymentInfo: string;
+    };
+  };
 }
 
-const PaymentAndDeliveryInfo: React.FC<PaymentAndDeliveryInfoProps> = ({dict}) => {
-
-
-
+const PaymentAndDeliveryInfo: React.FC<PaymentAndDeliveryInfoProps> = ({
+  dict,
+}) => {
+  const { header, paymentOptions, deliveryOptions } = dict;
+  const { subTitle, time, methods, paymentInfo } = deliveryOptions;
 
   return (
     <Section className={styles.paymentSection}>
       <Container>
+        <Typography variant="subheadingBold" className={styles.sectionTitle}>{header.title}</Typography>
+        <div className={styles.paymentWrapper}>
+          <div className={styles.paymentOptions}>
+            <div className={styles.iconCircle}>
+              <BsCreditCard className={styles.iconCard} />
+            </div>
+            <Typography
+              variant="bodyL"
+              color="var(--cl-primary-700)"
+              className={styles.paymentTitle}
+            >
+              {paymentOptions.subTitle}
+            </Typography>
+            <Typography variant="bodyRegular">
+              {paymentOptions.description}
+            </Typography>
+          </div>
 
-        
+          <div className={styles.deliveryOptions}>
+            <div className={styles.iconCircle}>
+              <BsBoxSeam className={styles.iconBox} />
+            </div>
+            <Typography
+              variant="bodyL"
+              color="var(--cl-primary-700)"
+              className={styles.paymentTitle}
+            >
+              {subTitle}
+            </Typography>
+            <Typography>{time}</Typography>
+
+            <div>
+              <Typography variant="bodyRegular">{methods.title}</Typography>
+              <ul>
+                {methods.availableMethods.map((method, index) => (
+                  <li key={index}>{method}</li>
+                ))}
+              </ul>
+            </div>
+
+            <Typography  className={styles.paymentInfo}>{paymentInfo}</Typography>
+          </div>
+        </div>
       </Container>
     </Section>
   );
