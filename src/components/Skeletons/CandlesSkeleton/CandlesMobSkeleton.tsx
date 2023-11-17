fe8b9a38012a19ede5a4fd8f9e432ece5658e508
ViraMesha <wira.mescha@gmail.com>
2023-11-17 12:31:36 +0200

    feat: Update radius in CandlesSkeleton

--- src/components/Skeletons/CandlesSkeleton/CandlesMobSkeleton.tsx
+++ src/components/Skeletons/CandlesSkeleton/CandlesMobSkeleton.tsx
@@ -11,12 +11,9 @@ const CandlesMobSkeleton = () => {
       backgroundColor="#f3f3f3"
       foregroundColor="#ecebeb"
     >
-      <rect x="25" y="481" rx="2" ry="2" width="140" height="10" />
       <rect x="0" y="0" rx="20" ry="20" width="154" height="183" />
-      <rect x="0" y="327" rx="0" ry="0" width="181" height="16" />
-      <rect x="0" y="351" rx="0" ry="0" width="90" height="16" />
-      <rect x="0" y="192" rx="0" ry="0" width="101" height="16" />
-      <rect x="0" y="215" rx="0" ry="0" width="80" height="16" />
+      <rect x="0" y="192" rx="2" ry="2" width="101" height="16" />
+      <rect x="0" y="215" rx="2" ry="2" width="80" height="16" />
     </ContentLoader>
   );
 };
--- src/components/Skeletons/CandlesSkeleton/CandlesTabLargeSkeleton.tsx
+++ src/components/Skeletons/CandlesSkeleton/CandlesTabLargeSkeleton.tsx
@@ -11,10 +11,9 @@ const CandlesTabLargeSkeleton = () => {
       backgroundColor="#f3f3f3"
       foregroundColor="#ecebeb"
     >
-      <rect x="25" y="481" rx="2" ry="2" width="140" height="10" />
       <rect x="0" y="0" rx="20" ry="20" width="224" height="248" />
-      <rect x="0" y="264" rx="0" ry="0" width="181" height="16" />
-      <rect x="0" y="292" rx="0" ry="0" width="90" height="16" />
+      <rect x="0" y="264" rx="2" ry="2" width="181" height="16" />
+      <rect x="0" y="292" rx="2" ry="2" width="90" height="16" />
     </ContentLoader>
   );
 };
