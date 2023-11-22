59599ac2719fc9352537cc40e64a5b965830e6fc
ViraMesha <wira.mescha@gmail.com>
2023-11-22 22:24:34 +0200

    bugfix: Fix location error in Cart

--- src/components/Header/Cart/Cart.tsx
+++ src/components/Header/Cart/Cart.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { BiShoppingBag } from 'react-icons/bi';
 import { toast } from 'react-toastify';
 
@@ -14,26 +14,30 @@ interface CartProps {
 
 const Cart: React.FC<CartProps> = ({ lang }) => {
   const { totalQuantities, cartItems } = useStateContext();
-  const router = useRouter();
 
   const handleCartClick = () => {
-    if (!cartItems.length) {
-      toast.warning(
-        'Your cart is empty. Please add items before proceeding to checkout.'
-      );
-    } else {
-      router.push(`/${lang}/checkout`);
-    }
+    toast.warning(
+      'Your cart is empty. Please add items before proceeding to checkout.'
+    );
   };
 
   return (
     <>
-      <button onClick={handleCartClick} className={styles.link}>
-        <BiShoppingBag />
-        {totalQuantities > 0 && (
-          <span className={styles.quantity}>{totalQuantities}</span>
-        )}
-      </button>
+      {cartItems.length ? (
+        <Link href={`/${lang}/checkout`}>
+          <BiShoppingBag />
+          {totalQuantities > 0 && (
+            <span className={styles.quantity}>{totalQuantities}</span>
+          )}
+        </Link>
+      ) : (
+        <button onClick={handleCartClick} className={styles.link}>
+          <BiShoppingBag />
+          {totalQuantities > 0 && (
+            <span className={styles.quantity}>{totalQuantities}</span>
+          )}
+        </button>
+      )}
     </>
   );
 };
