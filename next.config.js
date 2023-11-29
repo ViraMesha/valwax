579273723f24a7d0fb85785b9c662d362b495faa
Anton Shabalov <shabalovanton@gmail.com>
2023-11-29 14:29:57 +0200

    fix: unoptimized inst image

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-
-    domains: ['scontent-iev1-1.cdninstagram.com'],
-
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'scontent-iev1-1.cdninstagram.com',
-    //     // port: '',
-    //     // pathname: '',
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'scontent-iev1-1.cdninstagram.com',
+        // port: '',
+        // pathname: '',
+      },
+    ],
   },
 }
 
