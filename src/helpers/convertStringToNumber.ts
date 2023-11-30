7776e6d9b82b2037a29c99c06191d9c29a203285
ViraMesha <wira.mescha@gmail.com>
2023-12-01 00:32:07 +0200

    bugfix: Fix TS error in convertStringToNumber function

--- src/helpers/convertStringToNumber.ts
+++ src/helpers/convertStringToNumber.ts
@@ -1,7 +1,10 @@
-export const convertStringToNumber = (value, defaultValue) => {
-    if (typeof value === 'string') {
-      const convertedValue = Number(value);
-      return isNaN(convertedValue) ? defaultValue : convertedValue;
-    }
-    return defaultValue;
-  };
+export const convertStringToNumber = (
+  value: string | string[] | undefined,
+  defaultValue: number
+) => {
+  if (typeof value === 'string') {
+    const convertedValue = Number(value);
+    return isNaN(convertedValue) ? defaultValue : convertedValue;
+  }
+  return defaultValue;
+};
