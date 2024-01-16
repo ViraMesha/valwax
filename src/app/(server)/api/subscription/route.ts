20b4c05e3bb6cd3b440acfd4efb222a68819dbd3
ViraMesha <wira.mescha@gmail.com>
2024-01-16 21:30:26 +0200

    feat: Fix error handling in Subscription

--- src/app/(server)/api/subscription/route.ts
+++ src/app/(server)/api/subscription/route.ts
@@ -1,18 +1,40 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { BASE_URL } from '@components/constants';
+import { extractErrorMessage } from '@components/helpers/extractErrorMessage';
 
 export async function POST(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const email = searchParams.get('email');
   try {
-    await fetch(`${BASE_URL}/subscription/subscribe?email=${email}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/plain;charset=UTF-8',
-      },
-    });
+    const response = await fetch(
+      `${BASE_URL}/subscription/subscribe?email=${email}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain;charset=UTF-8',
+        },
+      }
+    );
+
+    if (String(response.status).includes('400')) {
+      return NextResponse.json(
+        { message: 'Already subscribed', success: false },
+        { status: 400 }
+      );
+    }
+
+    if (String(response.status).includes('404')) {
+      return NextResponse.json(
+        { message: 'Not Found', success: false },
+        { status: 404 }
+      );
+    }
+
     return NextResponse.json({ message: 'The email was sent', success: true });
   } catch (error: unknown) {
-    return NextResponse.json({ message: error, success: false });
+    return NextResponse.json(
+      { message: 'Internal server error', success: false },
+      { status: 500 }
+    );
   }
 }
